1c054f2b4b09744a61fa39a62eccc5f3c7632f12
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-02-12 13:42:21 +0530

    // neha

--- src/main/java/com/ats/rusasoft/controller/MasterController.java
+++ src/main/java/com/ats/rusasoft/controller/MasterController.java
@@ -1,5 +1,43 @@
 package com.ats.rusasoft.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Scope("session")
 public class MasterController {
+	RestTemplate rest = new RestTemplate();
+
+	@RequestMapping(value = "/showRegisterInstitute", method = RequestMethod.GET)
+	public ModelAndView showRegisterInstitute(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = null;
+		try {
+
+			model = new ModelAndView("master/reginstitute");
+
+			model.addObject("title", "Register Institute");
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
 
 }
