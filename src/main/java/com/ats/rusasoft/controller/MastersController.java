60b7ba1fe392077ce967fad067a0007f2d9f907b
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-02-12 15:00:29 +0530

    /Akshay

---
+++ src/main/java/com/ats/rusasoft/controller/MastersController.java
@@ -0,0 +1,54 @@
+package com.ats.rusasoft.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Scope("session")
+public class MastersController {
+	
+	@RequestMapping(value = "/iqacRegistration", method = RequestMethod.GET)
+	public ModelAndView showRegisterInstitute(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("master/iqacRegistration");
+		try {
+ 
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/hodRegistration", method = RequestMethod.GET)
+	public ModelAndView hodRegistration(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("master/hodRegistration");
+		try {
+ 
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+}
