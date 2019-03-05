71a456f48ddd4eedec46d3915155bbe817992e0a
krishna <krishna@192.168.2.8>
2019-03-05 01:42:20 -0800

    //harsha

--- src/main/java/com/ats/rusasoft/controller/MastersController.java
+++ src/main/java/com/ats/rusasoft/controller/MastersController.java
@@ -58,6 +58,7 @@ public class MastersController {
 	public ModelAndView hodList(HttpServletRequest request, HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("master/hodList");
+	
 		try {
  
 
@@ -77,12 +78,118 @@ public class MastersController {
 	public ModelAndView addFaculty(HttpServletRequest request, HttpServletResponse response) {
 
 		ModelAndView model = new ModelAndView("master/addFaculty");
+		model.addObject("title","Add Department");
 		try {
  
 
 		} catch (Exception e) {
 
 			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/showDeptList", method = RequestMethod.GET)
+	public ModelAndView showDeptList(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("master/deptList");
+		model.addObject("title","Department List");
+		try {
+ 
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+	
+	@RequestMapping(value = "/addPrincipal", method = RequestMethod.GET)
+	public ModelAndView addPrincipal(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("master/addPrincipal");
+		model.addObject("title","Add Principal");
+		try {
+ 
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/showPrincipalList", method = RequestMethod.GET)
+	public ModelAndView showPrincipalList(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("master/principalList");
+		model.addObject("title","Principal List");
+		try {
+ 
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showRegisterInstitute at Master Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/showPendingInstitute", method = RequestMethod.GET)
+ 	public ModelAndView showPendingInstitute(HttpServletRequest request, HttpServletResponse response) {
+
+ 		ModelAndView model = null;
+ 		try {
+
+ 			model = new ModelAndView("master/pendingInstituteList");
+
+ 			model.addObject("title", " Pending Institute");
+
+ 		} catch (Exception e) {
+
+ 			System.err.println("exception In showStaffList at Master Contr" + e.getMessage());
+
+ 			e.printStackTrace();
+
+ 		}
+
+ 		return model;
+
+ 	}
+  
+  @RequestMapping(value = "/showApprovedInstitute", method = RequestMethod.GET)
+	public ModelAndView showApprovedInstitute(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = null;
+		try {
+
+			model = new ModelAndView("master/approvedInstituteList");
+
+			model.addObject("title", " Pending Institute");
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showStaffList at Master Contr" + e.getMessage());
 
 			e.printStackTrace();
 
