db2b0d8bf5be26d5547c266b7e909f16d910f04d
Aaryatech <code@atsinfosoft.com>
2019-03-06 11:38:13 +0530

    acc..Right-Sac

---
+++ src/main/java/com/ats/rusasoft/model/accessright/AccessRightModuleList.java
@@ -0,0 +1,35 @@
+package com.ats.rusasoft.model.accessright;
+
+import java.util.List;
+
+import com.ats.rusasoft.model.Info;
+
+
+public class AccessRightModuleList {
+
+	List<AccessRightModule> accessRightModuleList;
+
+	Info info;
+
+	public List<AccessRightModule> getAccessRightModuleList() {
+		return accessRightModuleList;
+	}
+
+	public void setAccessRightModuleList(List<AccessRightModule> accessRightModuleList) {
+		this.accessRightModuleList = accessRightModuleList;
+	}
+
+	public Info getInfo() {
+		return info;
+	}
+
+	public void setInfo(Info info) {
+		this.info = info;
+	}
+
+	@Override
+	public String toString() {
+		return "AccessRightModuleList [accessRightModuleList=" + accessRightModuleList + ", info=" + info + "]";
+	}
+
+}
