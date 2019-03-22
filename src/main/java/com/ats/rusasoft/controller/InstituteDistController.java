f7fe4084510227344180026d51cacd9ac3d07741
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-03-22 16:09:24 +0530

    //neha

--- src/main/java/com/ats/rusasoft/controller/InstituteDistController.java
+++ src/main/java/com/ats/rusasoft/controller/InstituteDistController.java
@@ -31,6 +31,10 @@ import com.ats.rusasoft.model.LoginResponse;
 import com.ats.rusasoft.model.accessright.ModuleJson;
 import com.ats.rusasoft.model.instprofile.Disctinctveness;
 import com.ats.rusasoft.model.instprofile.GetDisctinctveness;
+import com.ats.rusasoft.model.instprofile.GetHumanValues;
+import com.ats.rusasoft.model.instprofile.GetResearchCenter;
+import com.ats.rusasoft.model.instprofile.HumanValues;
+import com.ats.rusasoft.model.instprofile.ResearchCenter;
 
 @Controller
 @Scope("session")
@@ -275,6 +279,490 @@ public class InstituteDistController {
 			map.add("distIdList", distId);
 			Info info = rest.postForObject(Constants.url + "/deleteDists", map, Info.class);
 			value = "redirect:/showProgDistinctive";
+		}
+		return value;
+
+	}
+
+	@RequestMapping(value = "/showHumanValues", method = RequestMethod.GET)
+	public ModelAndView showHumanValues(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView model = null;
+		try {
+
+			HttpSession session = request.getSession();
+
+			List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+
+			Info add = AccessControll.checkAccess("showHumanValues", "showHumanValues", "1", "0", "0", "0",
+					newModuleList);
+
+			if (add.isError() == false) {
+
+				model = new ModelAndView("instituteInfo/IQAC/humanValues");
+				model.addObject("title", "Human Values & Professional Ethics");
+
+				int instituteId = (int) session.getAttribute("instituteId");
+				int yId = (int) session.getAttribute("acYearId");
+
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+				map.add("instId", instituteId);
+				map.add("yearId", yId);
+
+				GetHumanValues[] valueArray = rest.postForObject(Constants.url + "/getAllHumanValuesByInstituteId", map,
+						GetHumanValues[].class);
+				List<GetHumanValues> valuelist = new ArrayList<>(Arrays.asList(valueArray));
+
+				model.addObject("valuelist", valuelist);
+
+				Info edit = AccessControll.checkAccess("showHumanValues", "showHumanValues", "0", "0", "1", "0",
+						newModuleList);
+				Info delete = AccessControll.checkAccess("showHumanValues", "showHumanValues", "0", "0", "0", "1",
+						newModuleList);
+
+				if (add.isError() == false) {
+					System.out.println(" add   Accessable ");
+					model.addObject("addAccess", 0);
+
+				}
+				if (edit.isError() == false) {
+					System.out.println(" edit   Accessable ");
+					model.addObject("editAccess", 0);
+				}
+				if (delete.isError() == false) {
+					System.out.println(" delete   Accessable ");
+					model.addObject("deleteAccess", 0);
+
+				}
+
+			} else {
+				model = new ModelAndView("accessDenied");
+			}
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showHumanValues at Institute Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/showAddHumanValues", method = RequestMethod.GET)
+	public ModelAndView showAddHumanValues(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView model = null;
+		try {
+
+			HttpSession session = request.getSession();
+
+			List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+
+			Info add = AccessControll.checkAccess("showAddHumanValues", "showHumanValues", "1", "0", "0", "0",
+					newModuleList);
+
+			if (add.isError() == false) {
+
+				model = new ModelAndView("instituteInfo/IQAC/add_human_values");
+
+				model.addObject("title", "Human Values & Professional Ethics");
+
+			} else {
+				model = new ModelAndView("accessDenied");
+			}
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showAddHumanValues at Institutedist Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/insertHumanValues", method = RequestMethod.POST)
+	public String insertHumanValues(HttpServletRequest request, HttpServletResponse response) {
+		String returnString = new String();
+		try {
+
+			HttpSession session = request.getSession();
+			List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+			Info view = AccessControll.checkAccess("showAddHumanValues", "showHumanValues", "0", "1", "0", "0",
+					newModuleList);
+
+			System.out.println(view);
+
+			if (view.isError() == false) {
+
+				System.err.println("Inside insertHumanValues method");
+
+				int valueId = 0;
+				try {
+					valueId = Integer.parseInt(request.getParameter("valueId"));
+				} catch (Exception e) {
+					valueId = 0;
+				}
+				int activityPcount = Integer.parseInt(request.getParameter("participant"));
+
+				String fromDate = request.getParameter("fromDate");
+				String toDate = request.getParameter("toDate");
+				String activityName = request.getParameter("title");
+				int instituteId = (int) session.getAttribute("instituteId");
+				int userId = (int) session.getAttribute("userId");
+				int yearId = (int) session.getAttribute("acYearId");
+
+				int is_view = Integer.parseInt(request.getParameter("is_view"));
+
+				HumanValues value = new HumanValues();
+				value.setDelStatus(1);
+				value.setValueId(valueId);
+				value.setExVar1("NA");
+				value.setExVar2("NA");
+				value.setInstituteId(instituteId);
+				value.setIsActive(1);
+				value.setMakerDatetime(dateTime);
+				value.setMakerUserId(userId);
+				value.setYearId(yearId);
+
+				value.setExInt1(1);
+				value.setExInt2(1);
+				value.setActivityFromdt(DateConvertor.convertToYMD(fromDate));
+				value.setActivityTodt(DateConvertor.convertToYMD(toDate));
+				value.setActivityName(activityName);
+				value.setActivityPcount(activityPcount);
+
+				HumanValues valueInsertRes = rest.postForObject(Constants.url + "saveHumanValues", value,
+						HumanValues.class);
+
+				System.err.println("valueInsertRes " + valueInsertRes.toString());
+
+				if (is_view == 1) {
+					returnString = "redirect:/showHumanValues";
+				} else {
+					returnString = "redirect:/showAddHumanValues";
+				}
+			} else {
+
+				returnString = "redirect:/accessDenied";
+
+			}
+		}
+
+		catch (Exception e) {
+			System.err.println("EXCE in valueInsertRes " + e.getMessage());
+			e.printStackTrace();
+
+		}
+		return returnString;
+
+	}
+
+	@RequestMapping(value = "/editValue/{valueId}", method = RequestMethod.GET)
+	public ModelAndView editValue(@PathVariable("valueId") int valueId, HttpServletRequest request) {
+
+		ModelAndView model = null;
+		HttpSession session = request.getSession();
+		List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+
+		try {
+			Info view = AccessControll.checkAccess("showAddHumanValues", "showHumanValues", "0", "0", "1", "0",
+					newModuleList);
+
+			if (view.isError() == true) {
+
+				model = new ModelAndView("accessDenied");
+
+			} else {
+
+				model = new ModelAndView("instituteInfo/IQAC/add_human_values");
+
+				model.addObject("title", "Edit Human Values & Professional Ethics");
+
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+				map.add("valueId", valueId);
+
+				HumanValues editValue = rest.postForObject(Constants.url + "/getHumanValuesByValueId", map,
+						HumanValues.class);
+				System.out.println("valueId:" + valueId);
+
+				model.addObject("editValue", editValue);
+
+			}
+		} catch (Exception e) {
+
+			System.err.println("exception In editDist at InstrituteDist Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+		return model;
+	}
+
+	@RequestMapping(value = "/deleteHumanValues/{valueId}", method = RequestMethod.GET)
+	public String deleteHumanValues(@PathVariable("valueId") int valueId, HttpServletRequest request) {
+		String value = null;
+		HttpSession session = request.getSession();
+		List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+		Info view = AccessControll.checkAccess("showAddHumanValues", "showHumanValues", "0", "0", "0", "1",
+				newModuleList);
+		if (view.isError() == true) {
+
+			value = "redirect:/accessDenied";
+
+		} else {
+
+			MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+			map.add("valueIdList", valueId);
+			Info info = rest.postForObject(Constants.url + "/deleteHumanVlaues", map, Info.class);
+			value = "redirect:/showHumanValues";
+		}
+		return value;
+
+	}
+
+	@RequestMapping(value = "/showResearchCenter", method = RequestMethod.GET)
+	public ModelAndView showResearchCenter(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView model = null;
+		try {
+
+			HttpSession session = request.getSession();
+
+			List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+
+			Info add = AccessControll.checkAccess("showResearchCenter", "showResearchCenter", "1", "0", "0", "0",
+					newModuleList);
+
+			if (add.isError() == false) {
+
+				model = new ModelAndView("instituteInfo/IQAC/list_research_center");
+				model.addObject("title", "Research Center");
+
+				int instituteId = (int) session.getAttribute("instituteId");
+				int yId = (int) session.getAttribute("acYearId");
+
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+				map.add("instId", instituteId);
+				map.add("yearId", yId);
+
+				GetResearchCenter[] researchArray = rest.postForObject(
+						Constants.url + "/getAllResearchCenterByInstituteId", map, GetResearchCenter[].class);
+				List<GetResearchCenter> centerlist = new ArrayList<>(Arrays.asList(researchArray));
+
+				model.addObject("centerlist", centerlist);
+
+				Info edit = AccessControll.checkAccess("showResearchCenter", "showResearchCenter", "0", "0", "1", "0",
+						newModuleList);
+				Info delete = AccessControll.checkAccess("showResearchCenter", "showResearchCenter", "0", "0", "0", "1",
+						newModuleList);
+
+				if (add.isError() == false) {
+					System.out.println(" add   Accessable ");
+					model.addObject("addAccess", 0);
+
+				}
+				if (edit.isError() == false) {
+					System.out.println(" edit   Accessable ");
+					model.addObject("editAccess", 0);
+				}
+				if (delete.isError() == false) {
+					System.out.println(" delete   Accessable ");
+					model.addObject("deleteAccess", 0);
+
+				}
+
+			} else {
+				model = new ModelAndView("accessDenied");
+			}
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showResearchCenter at Institute Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/showAddResearchCenter", method = RequestMethod.GET)
+	public ModelAndView showAddResearchCenter(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView model = null;
+		try {
+
+			HttpSession session = request.getSession();
+
+			List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+
+			Info add = AccessControll.checkAccess("showAddResearchCenter", "showResearchCenter", "1", "0", "0", "0",
+					newModuleList);
+
+			if (add.isError() == false) {
+
+				model = new ModelAndView("instituteInfo/IQAC/add_research_center");
+
+				model.addObject("title", "Add Research Center");
+
+			} else {
+				model = new ModelAndView("accessDenied");
+			}
+
+		} catch (Exception e) {
+
+			System.err.println("exception In showAddResearchCenter at Institutedist Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/insertResearchCenter", method = RequestMethod.POST)
+	public String insertResearchCenter(HttpServletRequest request, HttpServletResponse response) {
+		String returnString = new String();
+		try {
+
+			HttpSession session = request.getSession();
+			List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+			Info view = AccessControll.checkAccess("showAddResearchCenter", "showResearchCenter", "0", "1", "0", "0",
+					newModuleList);
+
+			System.out.println(view);
+
+			if (view.isError() == false) {
+
+				System.err.println("Inside insertResearchCenter method");
+
+				int rcId = 0;
+				try {
+					rcId = Integer.parseInt(request.getParameter("rcId"));
+				} catch (Exception e) {
+					rcId = 0;
+				}
+				int rcGuideCount = Integer.parseInt(request.getParameter("rc_guide_count"));
+				int rcStudentCount = Integer.parseInt(request.getParameter("rc_student_count"));
+
+				String fromDate = request.getParameter("fromDate");
+				String toDate = request.getParameter("toDate");
+				String rcSubjectName = request.getParameter("rc_subject_name");
+				String rcFacultyName = request.getParameter("rc_faculty_name");
+				int instituteId = (int) session.getAttribute("instituteId");
+				int userId = (int) session.getAttribute("userId");
+				int yearId = (int) session.getAttribute("acYearId");
+
+				int is_view = Integer.parseInt(request.getParameter("is_view"));
+
+				ResearchCenter value = new ResearchCenter();
+				value.setDelStatus(1);
+				value.setRcId(rcId);
+				value.setExVar1("NA");
+				value.setExVar2("NA");
+				value.setInstituteId(instituteId);
+				value.setIsActive(1);
+				value.setMakerDatetime(dateTime);
+				value.setMakerUserId(userId);
+				value.setYearId(yearId);
+				value.setRcFacultyName(rcFacultyName);
+				value.setRcSubjectName(rcSubjectName);
+				value.setRcValidityTodt(DateConvertor.convertToYMD(toDate));
+				value.setRcValidityFromdt(DateConvertor.convertToYMD(fromDate));
+				value.setRcStudentCount(rcStudentCount);
+				value.setRcGuideCount(rcGuideCount);
+
+				value.setExInt1(1);
+				value.setExInt2(1);
+
+				ResearchCenter valueInsertRes = rest.postForObject(Constants.url + "saveReserachCenter", value,
+						ResearchCenter.class);
+
+				System.err.println("valueInsertRes " + valueInsertRes.toString());
+
+				if (is_view == 1) {
+					returnString = "redirect:/showResearchCenter";
+				} else {
+					returnString = "redirect:/showAddResearchCenter";
+				}
+			} else {
+
+				returnString = "redirect:/accessDenied";
+
+			}
+		}
+
+		catch (Exception e) {
+			System.err.println("EXCE in valueInsertRes " + e.getMessage());
+			e.printStackTrace();
+
+		}
+		return returnString;
+
+	}
+
+	@RequestMapping(value = "/editResearchCenter/{rcId}", method = RequestMethod.GET)
+	public ModelAndView editResearchCenter(@PathVariable("rcId") int rcId, HttpServletRequest request) {
+
+		ModelAndView model = null;
+		HttpSession session = request.getSession();
+		List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+
+		try {
+			Info view = AccessControll.checkAccess("showAddResearchCenter", "showResearchCenter", "0", "0", "1", "0",
+					newModuleList);
+
+			if (view.isError() == true) {
+
+				model = new ModelAndView("accessDenied");
+
+			} else {
+
+				model = new ModelAndView("instituteInfo/IQAC/add_research_center");
+
+				model.addObject("title", "Add Research Center");
+
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+				map.add("rcId", rcId);
+
+				ResearchCenter editValue = rest.postForObject(Constants.url + "/getResearchCenterByRcId", map,
+						ResearchCenter.class);
+				System.out.println("rcId:" + rcId);
+
+				model.addObject("editValue", editValue);
+
+			}
+		} catch (Exception e) {
+
+			System.err.println("exception In editResearchCenter at InstrituteDist Contr" + e.getMessage());
+
+			e.printStackTrace();
+
+		}
+		return model;
+	}
+
+	@RequestMapping(value = "/deleteResearchCenter/{rcId}", method = RequestMethod.GET)
+	public String deleteResearchCenter(@PathVariable("rcId") int rcId, HttpServletRequest request) {
+		String value = null;
+		HttpSession session = request.getSession();
+		List<ModuleJson> newModuleList = (List<ModuleJson>) session.getAttribute("newModuleList");
+		Info view = AccessControll.checkAccess("showAddResearchCenter", "showResearchCenter", "0", "0", "0", "1",
+				newModuleList);
+		if (view.isError() == true) {
+
+			value = "redirect:/accessDenied";
+
+		} else {
+
+			MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+			map.add("rcIdList", rcId);
+			Info info = rest.postForObject(Constants.url + "/deleteResearchCenter", map, Info.class);
+			value = "redirect:/showResearchCenter";
 		}
 		return value;
 
