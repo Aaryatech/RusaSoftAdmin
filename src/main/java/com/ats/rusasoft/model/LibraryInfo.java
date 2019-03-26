453abf8b1e5244ecc2a8335bdef891eda16927ed
maddy <maddy@maddy-Inspiron-3541>
2019-03-26 17:04:05 +0530

    //maddy

---
+++ src/main/java/com/ats/rusasoft/model/LibraryInfo.java
@@ -0,0 +1,231 @@
+package com.ats.rusasoft.model;
+
+public class LibraryInfo {
+
+	private int libInfoId;
+	private int instituteId;
+	private int isLibAutomated;
+	private String softName;
+ 	private String softVersion;
+	private int usersOfLms;
+	private String dateOfPurchaseAutomation; //Date
+	private int noCompLan;
+	private String 	bandwidthForAccessingEresources;
+	private int isEresourceRemotly;
+	private float avgTeacher;
+	private float avgStudent;
+	private int addBy;
+	private String addDatetime;  
+	private int editBy;
+	private String editDatetime;
+	private int isPlanning;
+	private String dateOfPlanningEstablishment;  //Date
+	private int isAdministration;
+	private String dateOfAdministrationEstablishment;  //Date 
+	private int isFinanceAcc;
+	private String dateOfFinanceEstablishment;  //Date;
+	private int isStudentAdmition;
+	private String dateOfStudentEstablishment;  //Date
+	private int isExamination;
+	private String dateOfExaminationEstablishment;  //Date
+	private int delStatus;
+	private int exInt1;
+	private String exVar1;
+	public int getLibInfoId() {
+		return libInfoId;
+	}
+	public void setLibInfoId(int libInfoId) {
+		this.libInfoId = libInfoId;
+	}
+	public int getInstituteId() {
+		return instituteId;
+	}
+	public void setInstituteId(int instituteId) {
+		this.instituteId = instituteId;
+	}
+	public int getIsLibAutomated() {
+		return isLibAutomated;
+	}
+	public void setIsLibAutomated(int isLibAutomated) {
+		this.isLibAutomated = isLibAutomated;
+	}
+	public String getSoftName() {
+		return softName;
+	}
+	public void setSoftName(String softName) {
+		this.softName = softName;
+	}
+	public String getSoftVersion() {
+		return softVersion;
+	}
+	public void setSoftVersion(String softVersion) {
+		this.softVersion = softVersion;
+	}
+	public int getUsersOfLms() {
+		return usersOfLms;
+	}
+	public void setUsersOfLms(int usersOfLms) {
+		this.usersOfLms = usersOfLms;
+	}
+	public String getDateOfPurchaseAutomation() {
+		return dateOfPurchaseAutomation;
+	}
+	public void setDateOfPurchaseAutomation(String dateOfPurchaseAutomation) {
+		this.dateOfPurchaseAutomation = dateOfPurchaseAutomation;
+	}
+	public int getNoCompLan() {
+		return noCompLan;
+	}
+	public void setNoCompLan(int noCompLan) {
+		this.noCompLan = noCompLan;
+	}
+	public String getBandwidthForAccessingEresources() {
+		return bandwidthForAccessingEresources;
+	}
+	public void setBandwidthForAccessingEresources(String bandwidthForAccessingEresources) {
+		this.bandwidthForAccessingEresources = bandwidthForAccessingEresources;
+	}
+	public int getIsEresourceRemotly() {
+		return isEresourceRemotly;
+	}
+	public void setIsEresourceRemotly(int isEresourceRemotly) {
+		this.isEresourceRemotly = isEresourceRemotly;
+	}
+	public float getAvgTeacher() {
+		return avgTeacher;
+	}
+	public void setAvgTeacher(float avgTeacher) {
+		this.avgTeacher = avgTeacher;
+	}
+	public float getAvgStudent() {
+		return avgStudent;
+	}
+	public void setAvgStudent(float avgStudent) {
+		this.avgStudent = avgStudent;
+	}
+	public int getAddBy() {
+		return addBy;
+	}
+	public void setAddBy(int addBy) {
+		this.addBy = addBy;
+	}
+	public String getAddDatetime() {
+		return addDatetime;
+	}
+	public void setAddDatetime(String addDatetime) {
+		this.addDatetime = addDatetime;
+	}
+	public int getEditBy() {
+		return editBy;
+	}
+	public void setEditBy(int editBy) {
+		this.editBy = editBy;
+	}
+	public String getEditDatetime() {
+		return editDatetime;
+	}
+	public void setEditDatetime(String editDatetime) {
+		this.editDatetime = editDatetime;
+	}
+	public int getIsPlanning() {
+		return isPlanning;
+	}
+	public void setIsPlanning(int isPlanning) {
+		this.isPlanning = isPlanning;
+	}
+	public String getDateOfPlanningEstablishment() {
+		return dateOfPlanningEstablishment;
+	}
+	public void setDateOfPlanningEstablishment(String dateOfPlanningEstablishment) {
+		this.dateOfPlanningEstablishment = dateOfPlanningEstablishment;
+	}
+	public int getIsAdministration() {
+		return isAdministration;
+	}
+	public void setIsAdministration(int isAdministration) {
+		this.isAdministration = isAdministration;
+	}
+	public String getDateOfAdministrationEstablishment() {
+		return dateOfAdministrationEstablishment;
+	}
+	public void setDateOfAdministrationEstablishment(String dateOfAdministrationEstablishment) {
+		this.dateOfAdministrationEstablishment = dateOfAdministrationEstablishment;
+	}
+	public int getIsFinanceAcc() {
+		return isFinanceAcc;
+	}
+	public void setIsFinanceAcc(int isFinanceAcc) {
+		this.isFinanceAcc = isFinanceAcc;
+	}
+	public String getDateOfFinanceEstablishment() {
+		return dateOfFinanceEstablishment;
+	}
+	public void setDateOfFinanceEstablishment(String dateOfFinanceEstablishment) {
+		this.dateOfFinanceEstablishment = dateOfFinanceEstablishment;
+	}
+	public int getIsStudentAdmition() {
+		return isStudentAdmition;
+	}
+	public void setIsStudentAdmition(int isStudentAdmition) {
+		this.isStudentAdmition = isStudentAdmition;
+	}
+	public String getDateOfStudentEstablishment() {
+		return dateOfStudentEstablishment;
+	}
+	public void setDateOfStudentEstablishment(String dateOfStudentEstablishment) {
+		this.dateOfStudentEstablishment = dateOfStudentEstablishment;
+	}
+	public int getIsExamination() {
+		return isExamination;
+	}
+	public void setIsExamination(int isExamination) {
+		this.isExamination = isExamination;
+	}
+	public String getDateOfExaminationEstablishment() {
+		return dateOfExaminationEstablishment;
+	}
+	public void setDateOfExaminationEstablishment(String dateOfExaminationEstablishment) {
+		this.dateOfExaminationEstablishment = dateOfExaminationEstablishment;
+	}
+	public int getDelStatus() {
+		return delStatus;
+	}
+	public void setDelStatus(int delStatus) {
+		this.delStatus = delStatus;
+	}
+	public int getExInt1() {
+		return exInt1;
+	}
+	public void setExInt1(int exInt1) {
+		this.exInt1 = exInt1;
+	}
+	public String getExVar1() {
+		return exVar1;
+	}
+	public void setExVar1(String exVar1) {
+		this.exVar1 = exVar1;
+	}
+	@Override
+	public String toString() {
+		return "LibraryInfo [libInfoId=" + libInfoId + ", instituteId=" + instituteId + ", isLibAutomated="
+				+ isLibAutomated + ", softName=" + softName + ", softVersion=" + softVersion + ", usersOfLms="
+				+ usersOfLms + ", dateOfPurchaseAutomation=" + dateOfPurchaseAutomation + ", noCompLan=" + noCompLan
+				+ ", bandwidthForAccessingEresources=" + bandwidthForAccessingEresources + ", isEresourceRemotly="
+				+ isEresourceRemotly + ", avgTeacher=" + avgTeacher + ", avgStudent=" + avgStudent + ", addBy=" + addBy
+				+ ", addDatetime=" + addDatetime + ", editBy=" + editBy + ", editDatetime=" + editDatetime
+				+ ", isPlanning=" + isPlanning + ", dateOfPlanningEstablishment=" + dateOfPlanningEstablishment
+				+ ", isAdministration=" + isAdministration + ", dateOfAdministrationEstablishment="
+				+ dateOfAdministrationEstablishment + ", isFinanceAcc=" + isFinanceAcc + ", dateOfFinanceEstablishment="
+				+ dateOfFinanceEstablishment + ", isStudentAdmition=" + isStudentAdmition
+				+ ", dateOfStudentEstablishment=" + dateOfStudentEstablishment + ", isExamination=" + isExamination
+				+ ", dateOfExaminationEstablishment=" + dateOfExaminationEstablishment + ", delStatus=" + delStatus
+				+ ", exInt1=" + exInt1 + ", exVar1=" + exVar1 + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
